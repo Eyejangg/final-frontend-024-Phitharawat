3198bb5c1bc94a29470d8c0d891b3dda333efccb
Eyejangg <ptwptw1550@gmail.com>
2024-11-05 16:20:00 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-kub",
+  base: "/final-frontend-024-Phitharawat",
 });
